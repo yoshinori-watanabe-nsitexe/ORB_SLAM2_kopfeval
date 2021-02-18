fc97c86a56848d0fbdd5fc912861a1f645d28410
NSITEXE WASHIDA Ryuei <ryuei_washida@scip.local>
2021-02-18 18:13:48 +0900

    fix spec/defines.mk

--- spec/defines.mk
+++ spec/defines.mk
@@ -1,9 +1,9 @@
 TARGET:=ORB_SLAM
 PLATFORM:=nsitexe_dr1000c
 
-USER_INIT:=export LD_LIBRARY_PATH=/opt/gcc/6.2.0/linux/gcc-6.2.0_64/lib64:${LD_LIBRARY_PATH};pushd  ../ORB_SLAM2 ; ./build_orb_slam2.sh ;popd
-USER_BUILD:=export LD_LIBRARY_PATH=/opt/gcc/6.2.0/linux/gcc-6.2.0_64/lib64:${LD_LIBRARY_PATH};pushd  ../ORB_SLAM2 ; ./build_g2o.sh; ./build_DBow2.sh ;popd
-USER_RUN:=cd  ../ORB_SLAM2 ; ./Examples/Monocular/mono_tum Vocabulary/ORBvoc.txt Examples/Monocular/TUM1.yaml dataset/rgbd_dataset_freiburg1_rpy
+USER_INIT:=
+USER_BUILD:=pushd  ../ORB_SLAM2 ; ./build.sh ;popd
+USER_RUN:=cd  ../ORB_SLAM2 ; ./Examples/Monocular/mono_tum Vocabulary/ORBvoc.txt Examples/Monocular/TUM1.yaml ../dataset/rgbd_dataset_freiburg1_rpy
 USER_SLX_MODE_SWITCH:=cd  ../ORB_SLAM2 ; ./clean.sh
 OPTIMISTIC_ANALYSIS:=0
 CANDIDATE_THRESHOLD:=50
