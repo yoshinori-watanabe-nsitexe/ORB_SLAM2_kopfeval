1733a7481bf8adc51bedb6b6a0373a3e5eaf387e
NSITEXE WASHIDA Ryuei <ryuei_washida@scip.local>
2021-03-01 15:16:00 +0900

    Fix clean.sh

--- clean.sh
+++ clean.sh
@@ -8,56 +8,8 @@ pushd eigen
   echo "Cleaning eigen3 ..."
   if [ -d build ]; then
     pushd build
-      pushd 3rdparty
-        pushd libjpeg-turbo
-          ${CMAKE_CLEAN}
-        popd
-        pushd libtiff
-          ${CMAKE_CLEAN}
-        popd
-        pushd libwebp
-          ${CMAKE_CLEAN}
-        popd
-        pushd openjpeg/openjp2
-          ${CMAKE_CLEAN}
-        popd
-        pushd libpng
-          ${CMAKE_CLEAN}
-        popd
-        pushd openexr
-          ${CMAKE_CLEAN}
-        popd
-        pushd ippiw
-          ${CMAKE_CLEAN}
-        popd
-        pushd protobuf
-          ${CMAKE_CLEAN}
-        popd
-        pushd quirc
-          ${CMAKE_CLEAN}
-        popd
-        pushd ittnotify
-          ${CMAKE_CLEAN}
-        popd
-      popd
-      pushd modules
-        ${CMAKE_CLEAN}
-        pushd core
-          ${CMAKE_CLEAN}
-        popd
-        pushd flann
-          ${CMAKE_CLEAN}
-        popd
-        pushd imgproc
-          ${CMAKE_CLEAN}
-        popd
-        pushd ml
-          ${CMAKE_CLEAN}
-        popd
-      popd
-      pushd apps/version
-        ${CMAKE_CLEAN}
-      popd
+      ${CMAKE_CLEAN}
+    popd
   fi
 popd
 
@@ -67,11 +19,59 @@ pushd eigen
     rm -f lib/*.a
     if [ -d build ]; then
       pushd build
-        ${CMAKE_CLEAN}
         rm -f lib/*.a
+        ${CMAKE_CLEAN}
         pushd 3rdparty
+          rm -f ippicv/ippicv_lnx/icv/lib/intel64/libippicv.a
           rm -f lib/*.a
-          rm -f 
+          pushd libjpeg-turbo
+            ${CMAKE_CLEAN}
+          popd
+          pushd libtiff
+            ${CMAKE_CLEAN}
+          popd
+          pushd libwebp
+            ${CMAKE_CLEAN}
+          popd
+          pushd openjpeg/openjp2
+            ${CMAKE_CLEAN}
+          popd
+          pushd libpng
+            ${CMAKE_CLEAN}
+          popd
+          pushd openexr
+            ${CMAKE_CLEAN}
+          popd
+          pushd ippiw
+            ${CMAKE_CLEAN}
+          popd
+          pushd protobuf
+            ${CMAKE_CLEAN}
+          popd
+          pushd quirc
+            ${CMAKE_CLEAN}
+          popd
+          pushd ittnotify
+            ${CMAKE_CLEAN}
+          popd
+        popd
+        pushd modules
+          ${CMAKE_CLEAN}
+          pushd core
+            ${CMAKE_CLEAN}
+          popd
+          pushd flann
+            ${CMAKE_CLEAN}
+          popd
+          pushd imgproc
+            ${CMAKE_CLEAN}
+          popd
+          pushd ml
+            ${CMAKE_CLEAN}
+          popd
+        popd
+        pushd apps/version
+          ${CMAKE_CLEAN}
         popd
       popd
     fi
