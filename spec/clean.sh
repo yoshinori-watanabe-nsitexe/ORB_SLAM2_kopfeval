7ebe3cdfbd36623bee2929c7b04c1866e4ec1eca
NSITEXE WASHIDA Ryuei <ryuei_washida@scip.local>
2021-03-09 20:36:54 +0900

    fix clean.sh defines.mk

--- spec/clean.sh
+++ spec/clean.sh
@@ -7,30 +7,15 @@ CMAKE_CLEAN='rm -rf CMakeCache.txt cmake_install.cmake CMakeFiles Makefile'
 pushd ../ORB_SLAM2
   pushd Thirdparty/DBoW2
     echo "Cleaning Thirdparty/DBoW2 ..."
-    rm -f lib/libDBoW2.so
-    rm -f lib/libDBoW2.a
-    if [ -d build ]; then
-      pushd build
-        ${CMAKE_CLEAN}
-      popd
-    fi
+    rm -rf lib
+    rm -rf build
   popd
   pushd Thirdparty/g2o
     echo "Cleaning Thirdparty/g2o ..."
-    rm -f lib/libg2o.so
-    rm -f lib/libg2o.a
-    if [ -d build ]; then
-      pushd build
-        ${CMAKE_CLEAN}
-      popd
-    fi
+    rm -rf lib
+    rm -rf build
   popd
   echo "Cleaning ORB_SLAM2 ..."
-  rm -f lib/libORB_SLAM2.so
-  rm -f lib/libORB_SLAM2.a
-  if [ -d build ]; then
-    pushd build
-      ${CMAKE_CLEAN}
-    popd
-  fi
+  rm -rf lib
+  rm -rf build
 popd
--- spec/defines.mk
+++ spec/defines.mk
@@ -1,7 +1,7 @@
 TARGET:=ORB_SLAM
 
-USER_INIT:=
-USER_BUILD:=pushd ../; ./build_eigen.sh; ./build_opencv.sh; popd; pushd ../ORB_SLAM2/; ./build_DBoW2.sh; ./build_g2o.sh; ./build_ORB_SLAM.sh; popd
+USER_INIT:=pushd ../; ./build_eigen.sh; ./build_opencv.sh; popd; pushd ../ORB_SLAM2/; ./build_DBoW2.sh; ./build_g2o.sh; popd
+USER_BUILD:=pushd ../ORB_SLAM2/; ./build_ORB_SLAM.sh; popd
 USER_RUN:=cd  ../ORB_SLAM2 ; Examples/Monocular/mono_tum Vocabulary/ORBvoc.txt Examples/Monocular/TUM1.yaml ../dataset/rgbd_dataset_freiburg1_rpy
 USER_SLX_MODE_SWITCH:=rm -rf ../ORB_SLAM2/Examples/Monocular/mono_tum
 OPTIMISTIC_ANALYSIS:=0
@@ -10,11 +10,11 @@ FILTER_RATIO:=0.01
 FIND_DLP:=1
 FIND_PLP:=0
 FIND_TLP:=0
-FIND_OFL:=1
+FIND_OFL:=0
 TRACE_REMOTELY:=0
 VECTOR_CONTENTION:=0
 BASEPATH:=../ORB_SLAM2
-PTHREADS_PROGRAM:=0
+PTHREADS_PROGRAM:=1
 SCALAR_CONTENTION:=0
 ENABLE_AUTO_VECTORIZATION:=0
 USER_CLEAN:=pushd ../; ./clean.sh; popd; ./clean.sh
