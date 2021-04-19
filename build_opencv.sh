fd0fff4696171799c850e8366bd0cc00863ee0ac
ryuei-washida-nsitexe <ryuei_washida@nsitexe.co.jp>
2021-04-19 18:57:06 +0900

    update build_opencv.sh

--- build_opencv.sh
+++ build_opencv.sh
@@ -1,5 +1,4 @@
-  
-#!/bin/bash
+  #!/bin/bash
 # make sure the script fails if there is an error
 set -e
 
@@ -10,7 +9,7 @@ pushd eigen
     mkdir -p build
     pushd build
       echo "Starting cmake for openCV 2.4 ..."
-      cmake -DBUILD_SHARED_LIBS=OFF -DENABLE_PROFILING=ON -DEIGEN_INCLUDE_PATH=../../../eigen3/include/eigen3 ../
+      cmake -DBUILD_SHARED_LIBS=OFF -DEIGEN_INCLUDE_PATH=../../../eigen3/include/eigen3 ../
       echo "Starting make for openCV 2.4 ..."
       make -j4
     popd
